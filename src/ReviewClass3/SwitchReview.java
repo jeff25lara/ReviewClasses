3d2866cf1e99a05945b64cdc3e43bb8b992d76ec
jeff25lara <jeff25lara@gmail.com>
2023-01-12 20:34:51 -0500

    Commit ReviewClasses

---
+++ src/ReviewClass3/SwitchReview.java
@@ -0,0 +1,24 @@
+package ReviewClass3;
+
+public class SwitchReview {
+    public static void main(String[] args) {
+        /*
+        World Cup:
+         */
+        int worldCup = 2022;
+        switch (worldCup){
+            case 2022:
+                System.out.println("Winner is Argentina");
+                break;
+            case 2018:
+                System.out.println("Winner is France");
+                break;
+            case 2014:
+                System.out.println("Winner is Germany");
+                break;
+            default:
+                System.out.println("Winner is unknownc");
+        }
+    }
+
+}
---
+++ src/ReviewClass3/reviewClass3.java
@@ -0,0 +1,16 @@
+package ReviewClass3;
+
+public class reviewClass3 {
+    public static void main(String[] args) {
+
+        boolean loginButtonDisplayed=true;
+        boolean loginButtonClickable=true;
+
+        if(loginButtonDisplayed&&loginButtonClickable){
+            System.out.println("Test case is passed");
+        }else{
+            System.out.println("Test case failed");
+        }
+
+    }
+}
